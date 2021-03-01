53aa3686bd321ef04ea08df867ce0e3d83e127dd
vaishnavipy <ravichandran.vaishu@gmail.com>
2021-03-01 15:48:16 -0600

    dependencies added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { ApolloClient,ApolloCache, InMemoryCache} from "@apollo/client";
+
+
 
 function App() {
+
+
+
+  const client = new ApolloClient({
+    link : "https://graphql-pokemon2.vercel.app/",
+    cache: new InMemoryCache()
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     
     </div>
   );
 }
